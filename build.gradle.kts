a682738b5f21de005c0144aea32d4312eabd4a90
thosea <104597976+imthosea@users.noreply.github.com>
2026-01-21 22:01:58 -0500

    move LWJGL setup to main gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,32 @@ plugins {
 	`java-library`
 	`maven-publish`
 	id("buildSrc-gdx-setup")
-	id("buildSrc-lwjgl")
+}
+
+repositories {
+	mavenCentral()
 }
 
 dependencies {
 	api(libs.jetbrains.annotations)
 	api(libs.joml)
+
+	api(platform(libs.lwjgl))
+	api("org.lwjgl:lwjgl")
+	api("org.lwjgl:lwjgl-freetype")
+	api("org.lwjgl:lwjgl-egl")
+	api("org.lwjgl:lwjgl-glfw")
+	api("org.lwjgl:lwjgl-opengl")
+	api("org.lwjgl:lwjgl-stb")
+	for(platform in listOf(
+		"natives-linux", "natives-linux-arm64", "natives-linux-arm32",
+		"natives-macos", "natives-macos-arm64",
+		"natives-windows", "natives-windows-arm64", "natives-windows-x86"
+	)) {
+		api("org.lwjgl:lwjgl") { artifact { classifier = platform } }
+		api("org.lwjgl:lwjgl-freetype") { artifact { classifier = platform } }
+		api("org.lwjgl:lwjgl-glfw") { artifact { classifier = platform } }
+		api("org.lwjgl:lwjgl-opengl") { artifact { classifier = platform } }
+		api("org.lwjgl:lwjgl-stb") { artifact { classifier = platform } }
+	}
 }
